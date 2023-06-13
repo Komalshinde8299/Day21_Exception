d15aa124ed26c81604cb72bd14d4e22070b1fc34
Komal Shinde <komalshinde8299@gmail.com>
2023-06-13 12:06:03 +0530

    Added:Code for analyze happy or sad mood from msg  is added

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+
+public class MoodAnalyzer {
+
+    public String analyseMood(String msg){
+        msg=msg.toLowerCase();
+
+        if(msg.contains("happy")){
+            return "Happy";
+        } else if (msg.contains("sad")) {
+            return "Sad";
+
+        }else {
+            return "Not Valid";
+        }
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        moodAnalyzer.analyseMood("I am Happy");
+
+    }
+
+}
